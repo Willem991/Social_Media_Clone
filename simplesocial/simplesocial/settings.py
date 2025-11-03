4d107e1700f05b10f3f69f8550ed1f92b630779a
Willem Nieuwenhuis <ruan.nieu@gmail.com>
2025-11-03 09:31:25 +0200

    Added in login signup and logout

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'simplesocial'
+    'simplesocial',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
