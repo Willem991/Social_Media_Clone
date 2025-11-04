ed8d50370a2a0c6f8653b31d68da8306208a1cf7
Willem Nieuwenhuis <ruan.nieu@gmail.com>
2025-11-04 15:55:52 +0200

    Fixed template and static bug

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'simplesocial',
     'bootstrap3'
 ]
 
@@ -124,3 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
